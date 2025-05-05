5878b7faf8e17de84bc9acd6ee9f33233e31472d
yashwanth1204 <kyreddy1204@gmail.com>
2025-05-05 16:53:40 -0500

    donee

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/Project-final/",   // 👈 THIS is the fix!
   plugins: [react(), tailwindcss()],
 })
